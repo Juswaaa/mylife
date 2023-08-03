2e361401dfb0eafd712a621c97b2f50b39acdf6f
Juswaaa <72263767+Juswaaa@users.noreply.github.com>
2023-08-03 23:04:32 +0800

    Update my_life.py

--- my_life.py
+++ my_life.py
@@ -1,1 +1,8 @@
+def write_multiple_lines_to_file(output_file):
+    with open(output_file, 'w') as file:
+        while True:
+            line = input("Enter line: ")
+            file.write(line + "\n")
 
+            more_lines = input("Are there more lines? (y/n): ").lower()
+            if more_lines != 'y':
